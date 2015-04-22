009c6352d0a47a560371b3778b41b0248327d550
tamaraf <tamarafox.sa@gmail.com>
2015-04-21 19:38:10 -0600

    finished mini project

---
+++ app.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+	$('.box').on('click', function() {
+		$(this).addClass(color);
+	})
+
+	$('.box').on('dblclick', function() {
+		$(this).removeClass(color);
+	})
+
+	$('#reset').on('click', function() {
+		$('.box').removeClass('red blue green yellow white')
+	})
+
+	var color = 'white';
+	$('#red').on('click', function() {
+		color = 'red';
+	})
+	$('#blue').on('click', function() {
+		color = 'blue';
+	})
+	$('#green').on('click', function() {
+		color = 'green';
+	})
+	$('#yellow').on('click', function() {
+		color = 'yellow';
+	})
+	$('#white').on('click', function() {
+		color = 'white';
+	})
+	 var colors = 'white green red blue yellow'
+
+  $('.box').on('dblclick', function() {
+    $(this).removeClass(colors);
+  })
+
+  $('#reset').on('click', function() {
+    $('.box').removeClass(colors)
+  })
+});
